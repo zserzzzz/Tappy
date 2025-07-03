1d4c00710290446c689c8eef08e523bf03705026
zserzzzz <gam3r.zaka@gmail.com>
2025-07-03 13:54:25 +0100

    anim done!

--- Scenes/Plane/character_body_2d.gd
+++ Scenes/Plane/character_body_2d.gd
@@ -1,12 +1,16 @@
 extends CharacterBody2D
 
 var gravity: float = ProjectSettings.get("physics/2d/default_gravity")
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("jump"):
 		print("jumped")
 		velocity.y = -500
+		if animation_player.is_playing():
+			animation_player.stop()
+		animation_player.play("jump")
 
 func _physics_process(delta: float) -> void:
 	velocity.y += gravity * delta
