c76710c2b6c7d8e4d599816617ee4e0144283594
zserzzzz <gam3r.zaka@gmail.com>
2025-07-03 13:32:39 +0100

    b4 plane movement solotuin
    cant spell for the love of god 🙏

--- Scenes/Plane/character_body_2d.gd
+++ Scenes/Plane/character_body_2d.gd
@@ -1,11 +1,12 @@
 extends CharacterBody2D
 
+var gravity: float = ProjectSettings.get("physics/2d/default_gravity")
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta: float) -> void:
+	velocity.y += gravity * delta
+	move_and_slide()
